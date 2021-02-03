caa7e404e42a676101e5e247441f8afb9f80c5ab
eb110 <wfigura@op.pl>
2021-02-03 23:25:51 +0000

    hor disp elements plus map fragment implementation

--- app/src/main/java/com/example/bgs_figura/BGS_Event_Figura.java
+++ app/src/main/java/com/example/bgs_figura/BGS_Event_Figura.java
@@ -7,11 +7,17 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bgs_figura.data.Earthquake;
+import com.google.android.gms.maps.GoogleMap;
 
 public class BGS_Event_Figura extends AppCompatActivity {
 
-    TextView textView;
+    TextView tv_date;
+    TextView tv_magnitude;
+    TextView tv_depth;
+    TextView tv_latlong;
     Earthquake quake = new Earthquake();
+
+    private GoogleMap mMap;
 
     public void setQuake (Earthquake quake){
 
@@ -22,11 +28,20 @@ public class BGS_Event_Figura extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b_g_s__event__figura);
 
-        textView = (TextView) findViewById(R.id.textView);
+        tv_date = (TextView) findViewById(R.id.tv_date);
+        tv_magnitude = (TextView) findViewById(R.id.tv_magnitude);
+        tv_depth = (TextView) findViewById(R.id.tv_depth);
+        tv_latlong = (TextView) findViewById(R.id.tv_latlong);
 
         Intent intent = getIntent();
         quake = (Earthquake) intent.getSerializableExtra("quake");
 
-        textView.setText(quake.getLocation());
+        tv_date.setText(quake.getDate());
+        tv_magnitude.setText("Magnitude: " + quake.getMagnitude() + " Richter Scale");
+        tv_depth.setText(("Depth: " + quake.getDepth() + " km"));
+        tv_latlong.setText(("Latitude: " + quake.getLatitude() + " Longitude: " +quake.getLongitude()));
+
     }
+
+
 }
